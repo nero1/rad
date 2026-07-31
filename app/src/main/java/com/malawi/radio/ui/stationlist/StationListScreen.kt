77af2a1409e8a14c04dfce40ce555d873359da68
Robot King <45537500+nero1@users.noreply.github.com>
2026-07-31 19:03:05 +0100

    Increase padding for search input field

--- app/src/main/java/com/malawi/radio/ui/stationlist/StationListScreen.kt
+++ app/src/main/java/com/malawi/radio/ui/stationlist/StationListScreen.kt
@@ -78,7 +78,7 @@ fun StationListScreen(
                     },
                     modifier = Modifier
                         .weight(1f)
-                        .padding(end = 1.dp)
+                        .padding(end = 5.dp)
                         .focusRequester(searchFocusRequester),
                     singleLine = true,
                     placeholder = { Text("Search stations") },
